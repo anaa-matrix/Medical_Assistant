afc997be7452dea047c6f701272f0452979b2ac1
anaa-matrix <114763190+anaa-matrix@users.noreply.github.com>
2024-05-01 19:18:43 +0530

    Update recommendation.js

--- controllers/recommendation.js
+++ controllers/recommendation.js
@@ -1,19 +1,38 @@
-const Doctor = require('../models/doctor');
+const Doctor = require('../models/Doctor');
 
 const recommendation = async (req, res) => {
-  try {
-    const { speciality } = req.body;
+    try {
+        const { speciality, latitude, longitude } = req.body;
 
-    // Find doctors with the specified speciality
-    const doctors = await Doctor.find({ speciality })
-      .sort({ rating: -1 }) // Sort doctors by rating in descending order
-      .limit(5); // Limit the result to 5 top-rated doctors
+        // Find doctors with the specified speciality and calculate the distance
+        const doctors = await Doctor.aggregate([
+            {
+                $geoNear: {
+                    near: {
+                        type: 'Point',
+                        coordinates: [longitude, latitude] // The user's location
+                    },
+                    distanceField: 'distance',
+                    spherical: true,
+                    maxDistance: 10000 // Maximum distance in meters (adjust as needed)
+                }
+            },
+            {
+                $match: { speciality }
+            },
+            {
+                $sort: { distance: 1, 'rating': -1 } // Sort by distance and then by rating
+            },
+            {
+                $limit: 15 // Limit the result to 15 doctors
+            }
+        ]);
 
-    res.status(200).json({ message: 'Recommendation generated successfully', doctors });
-  } catch (error) {
-    console.error('Error generating recommendation:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
+        res.status(200).json({ message: 'Recommendation generated successfully', doctors });
+    } catch (error) {
+        console.error('Error generating recommendation:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
 };
 
 module.exports = recommendation;
